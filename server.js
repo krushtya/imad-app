7313f9bd15887dbc6c55b6e10389791970e9c1f7
krushtya <36431616+krushtya@users.noreply.github.com>
2018-02-19 16:26:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-//source code of web server which actually runs on web server
+//source code of web server, which actually runs on web server
+//software packages
 var express = require('express');//'express' library is used to create web server.Used for handing http connections,learning how to access                                         ports
 var morgan = require('morgan');//'morgan' library help us output logs of servers that we know what request are comming to a server and how we are responding(i.e how server responds us)
 var path = require('path');
@@ -6,8 +7,8 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+app.get('/', function (req, res) {//'get' request make to '/',the given function is executed
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));//'sendFile' function is used to pickup the file UI/INDEX.HTML which is available to us and we send the content of that file
 });
 
 app.get('/ui/style.css', function (req, res) {
