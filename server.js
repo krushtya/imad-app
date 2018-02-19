1828aad8b3fd0c8631b5f6baabee8a46d5ba70bd
krushtya <36431616+krushtya@users.noreply.github.com>
2018-02-19 16:59:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.get('/', function (req, res) {//'get' request make to '/' so that the given 
 });
 
 app.get('/article1',function(req,res){
-   res.sendFile(path.join(_dirname,'ui','article1.html')); 
+   res.sendFile(path.join(__dirname,'ui','article1')); 
 });
 
 app.get('/ui/style.css', function (req, res) {
