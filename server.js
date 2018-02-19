a3b951d71bf855c8468ae3ad36eeb713013a2ce3
krushtya <36431616+krushtya@users.noreply.github.com>
2018-02-19 17:02:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ app.get('/', function (req, res) {//'get' request make to '/' so that the given 
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));//'sendFile' function is used to pickup the file UI/INDEX.HTML which is available to us and we send the content of that file
 });
 
-app.get('/ui/article1',function(req,res){
-   res.sendFile(path.join(__dirname,'ui','article1')); 
+app.get('/ui/article1.html',function(req,res){
+   res.sendFile(path.join(__dirname,'ui','article1.html')); 
 });
 
 app.get('/ui/style.css', function (req, res) {
