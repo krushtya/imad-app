7d552cff652edf49263e1e2f63760f20552fc810
krushtya <36431616+krushtya@users.noreply.github.com>
2018-02-20 09:12:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,13 +1,16 @@
 console.log('Loaded!');
 var img = document.getElementById('madi');
 var marginLeft = 0;
+var marginRight = 500;
 var interval;
 function moveRight(){
     marginLeft = marginLeft + 25;
+    marginRight=marginRight-25;
     img.style.marginLeft = marginLeft + "px";//concatnation
 }
 function moveLeft(){
     marginRight = marginRight + 25;
+     marginLeft =  marginLeft-25;
     img.style.marginRight = marginRight + "px";//concatnation
 }
 img.onclick = function(){
