1e3064ff307e708797af97cab1052b49e7e65586
krushtya <36431616+krushtya@users.noreply.github.com>
2018-02-20 10:57:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,26 +1,8 @@
-<html>
-    <head>
-        <link href="/ui/style.css" rel="stylesheet" />
-    </head>
-    <body>
-        <div class="container">
-        <div class="center">
-            <img id="madi" style="-webkit-user-select: none;background-position: 0px 0px, 10px 10px;background-size: 20px 20px;background-image:linear-gradient(45deg, #eee 25%, transparent 25%, transparent 75%, #eee 75%, #eee 100%),linear-gradient(45deg, #eee 25%, white 25%, white 75%, #eee 75%, #eee 100%);cursor: zoom-in;" src="https://scontent.fbom16-1.fna.fbcdn.net/v/t1.0-9/22554961_875832872592207_8310837890060334198_n.jpg?oh=533e1eb4886d8adb8b6d8e198938303a&amp;oe=5B08BE71" width="200" height="200">
-        </div>
-        <hr/>
-        <br>
-        <div class="center text-big bold">
-          Name:Pritam Kore
-          <hr/>
-        </div>
-        <div class="footer">
-             This button <button id="counter">Click me!</button> has been clicked <span id="count">0</span> times 
-        </div>
-        </div>
-        <script>
-            alert('hii I\'m your assistant');//inlin1e scripting
-        </script>
-        <script type="text/javascript" src="/ui/main.js">
-        </script>
-    </body>
-</html>
+//counter code
+var button = document.getElementById('counter');
+var counter=0;
+button.onclick = function(){
+    counter = counter+1;
+    var span = document.getElementById('count');
+    span.innerHTML=counter.toString();
+};
