2118a89e1b4240d144f024a7f6d4cfc934101f16
krushtya <36431616+krushtya@users.noreply.github.com>
2018-02-20 16:20:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -30,15 +30,15 @@ button.onclick = function(){
 var nameInput=document.getElementById('name');
 var name=nameInput.value;
 var submit=document.getElementById('submit_btn');
-submit.onclick = function(){
+submit.onclick = function(null){
    // make a request to a serer and send the name
    
    //capture a list of names and render it as a list 
      var names=['name1','name2','name3'];
-   /*  var list ='';
+    var list ='';
      for(var i=0;i<names.length;i++){
          list += '<li>'+names[i]+'</li>';
-     }*/
+     }
     var ul=document.getElementById('namelist');
     ul.innerHTML = list;
 };
