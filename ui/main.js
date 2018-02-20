79729440ce3bba848be01dc19e62b7ec849e74ff
krushtya <36431616+krushtya@users.noreply.github.com>
2018-02-20 16:29:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -27,6 +27,7 @@ button.onclick = function(){
 };
 
 //submit name
+window.onload=function(){
 var nameInput=document.getElementById('name');
 var name=nameInput.value;
 var submit=document.getElementById('submit_btn');
@@ -41,4 +42,5 @@ submit.onload = function(){
      }
     var ul=document.getElementById('namelist');
     ul.innerHTML = list;
+};
 };
