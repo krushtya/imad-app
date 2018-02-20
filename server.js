732abbe646c3eb7bf9b3cd7182005bcf05afa556
krushtya <36431616+krushtya@users.noreply.github.com>
2018-02-20 10:26:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -29,7 +29,6 @@ app.get('/ui/madi.png', function (req, res) {
 
 var counter =0;
 app.get('/counter',function(req, res){
-    counter=0;
     counter =counter +1;
     res.send(counter.toString());
 });
