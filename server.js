b23c71448669120a35a214f6085140cc0780de93
krushtya <36431616+krushtya@users.noreply.github.com>
2018-03-02 17:00:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -36,8 +36,7 @@ app.get('/test-db',function(req,res){
         if(err){
             res.status(500).send(err.toString());
         } else{
-           var articleData=result.rows[0];
-               res.send(createTemplate(articleData));
+            res.send(JSON.stringify(result.rows));
         }
     });                              
 });
