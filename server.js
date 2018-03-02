b81ae55dd127ee296f19d3509a3abb69deda4b32
krushtya <36431616+krushtya@users.noreply.github.com>
2018-03-02 17:01:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ app.get('/articles/:articleName',function(req,res){
            if(result.rows.length===0){
                res.status(404).send('Article not found');
            }else{
-               res.send(JSON.stringify(result.rows));
+               res.send(JSON.stringify(result));
            }
        }
    });
